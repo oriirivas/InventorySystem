e76c04384943c8cb85fa17e04df996dd0ee203b4
Oriirivas <orianarivas7@gmail.com>
2019-12-19 11:43:43 -0300

    "add col"

---
+++ src/app/componets/nav-filter/nav-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-filter',
+  templateUrl: './nav-filter.component.html',
+  styleUrls: ['./nav-filter.component.css']
+})
+export class NavFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
