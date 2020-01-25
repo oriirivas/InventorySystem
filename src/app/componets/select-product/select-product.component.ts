06781e226ea354cc42d79780cf2825e53c34d6ba
Oriirivas <orianarivas7@gmail.com>
2020-01-25 01:09:19 -0300

    factura table

--- src/app/componets/select-product/select-product.component.ts
+++ src/app/componets/select-product/select-product.component.ts
@@ -22,13 +22,7 @@ export class SelectProductComponent implements OnInit {
 
   prductSale(){
     this.listProducts=this.productService.carritoSendInfo()
-    /*let obs= this.productService.carritoSendInfo();
-    obs.subscribe(res=> {
-      console.log(res);
-      this.listProducts=res;
-      console.log(this.listProducts);
-    });*/
-     
+         
   }
 
  
